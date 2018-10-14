1e3562c5f6147da9cfa5a0144f1a1ae81c0b190c
kongminhao <2015141463077@stu.scu.edu.cn>
2018-10-14 17:11:07 +0800

    Update install.sh
    add simple ubuntu install shell, not test

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+# if ubuntu, 先写ubuntu的
+
+function install_for_ubuntu(){
+	# install git ,tmux, zsh, vim,htop,nload
+	apt-get update && apt-get install git, tmux, zsh, vim, htop, nload，wget
+	if [ $? -eq 0 ];then
+		# install oh-my-zsh, repalce my favorate Themes
+		sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+		sed -i '' 's/robbyrussell/agnoster/g' ~/.zshrc
+		# 
+		cp .tmux.conf ~/.tmux.conf
+		cp basic.vim ~/.vimrc
+		tmux source ~/.tmux.conf
+	fi
+}
